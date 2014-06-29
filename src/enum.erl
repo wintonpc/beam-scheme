15268fa5bd5b47b923992e399eb4e66e9717c3c0
Peter Winton <pwinton@indigobio.com>
2014-06-29 18:12:22 -0400

    lazy enum to_list/from_list

--- src/enum.erl
+++ src/enum.erl
@@ -1,8 +1,11 @@
 -module(enum).
--export([make_enum/1, yield/1, next/1]).
+-export([make_enum/1, yield/1, next/1, done/0, to_list/1, from_list/1]).
 -include_lib("eunit/include/eunit.hrl").
 
-make_enum(G) -> {enum, spawn(G)}.
+make_enum(G) -> {enum, spawn(fun() ->
+                                     G(),
+                                    yield('ENUM_DONE')
+                             end)}.
 
 yield(V) ->
     receive
@@ -12,9 +15,33 @@ yield(V) ->
     end.
 
 next({enum, Pid}) ->
-    Pid ! {next, self()},
-    receive
-        {next_value, Pid, V} -> V
+    Alive = is_process_alive(Pid),
+    if
+        Alive ->
+            Pid ! {next, self()},
+            receive
+                {next_value, Pid, V} -> V
+            end;
+        true -> 'ENUM_DONE'
+    end.
+
+done() -> 'ENUM_DONE'.
+
+to_list(E) -> to_list(E, []).
+to_list(E, Acc) ->
+    case next(E) of
+        'ENUM_DONE' -> lists:reverse(Acc);
+        V -> to_list(E, [V|Acc])
+    end.
+
+from_list(List) ->
+    make_enum(fun() -> gen_from_list(List) end).
+
+gen_from_list(List) ->
+    case List of
+        [] -> ok;
+        [H|T] -> yield(H),
+                 gen_from_list(T)
     end.
 
 
@@ -55,6 +82,12 @@ gen_squares(E) ->
     yield(square(next(E))),
     gen_squares(E).
 
+
+%% four
+
+four() -> make_enum(fun() -> yield(1), yield(2), yield(3), yield(4) end).
+
+
 %% tests
 
 enum_test_() ->
@@ -93,4 +126,21 @@ compose_enum_test_() ->
      ?_assertEqual(25, next(Sf)),
      ?_assertEqual(64, next(Sf)),
      ?_assertEqual(169, next(Sf))
+    ].
+
+to_list_test_() ->
+    [
+     ?_assertEqual([1,2,3,4], to_list(four()))
+    ].
+
+from_list_test_() ->
+    [
+     ?_assertEqual([1,2,3,4], to_list(from_list([1,2,3,4])))
+    ].
+
+empty_enum_test_() ->
+    MakeEmpty = fun() -> make_enum(fun() -> ok end) end,
+    [
+     ?_assertEqual([], to_list(MakeEmpty())),
+     ?_assertEqual(done(), next(MakeEmpty()))
     ].
