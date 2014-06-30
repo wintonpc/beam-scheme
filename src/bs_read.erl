531eacfa4184086ecd06466fd1b4cb4177909643
Peter Winton <pwinton@indigobio.com>
2014-06-29 20:06:44 -0400

    Tokenize and evaluate sexps

--- src/bs_read.erl
+++ src/bs_read.erl
@@ -5,12 +5,9 @@
 
 
 read(S) ->
-    read_tokens(tokenize(S)).
+    evaluate(tokenize(S)).
 
 tokenize(S) ->
-    classify(raw_tokens(S)).
-
-raw_tokens(S) ->
     stream:make(fun() -> gen_tokenize(S, []) end).
 
 gen_tokenize(S, CurrentToken) ->
@@ -22,11 +19,11 @@ gen_tokenize(S, CurrentToken) ->
             case C of
                 $( ->
                     yield_token(CurrentToken),
-                    yield_token($(),
+                    yield_token("("),
                     gen_tokenize(Rest, []);
                 $) ->
                     yield_token(CurrentToken),
-                    yield_token($)),
+                    yield_token(")"),
                     gen_tokenize(Rest, []);
                 $ ->
                     yield_token(CurrentToken),
@@ -42,16 +39,15 @@ yield_token(T) ->
         _ -> stream:yield(lists:reverse(T))
     end.
 
-classify(TokenStream) ->
-    stream:map(TokenStream, fun(X) -> X end).
+evaluate(TokenStream) ->
+    stream:map(TokenStream, fun evaluate_token/1).
 
-read_tokens(Ts) ->
-    lists:map(fun read_token/1, Ts).
-
-read_token(T) ->
+evaluate_token(T) ->
     cond_([
+           ?WHEN(T == "("), ?THEN('('),
+           ?WHEN(T == ")"), ?THEN(')'),
            ?WHEN(tok_is_integer(T)), ?THEN(string_to_integer(T))
-          ], ?ELSE(error)).
+          ], ?ELSE(list_to_atom(T))).
 
 string_to_integer(S) ->
     {V, _} = string:to_integer(S),
@@ -73,20 +69,24 @@ is_digit(C) -> lists:member(C, "0123456789").
 
 % tests
 
-raw_tokenize_test_() ->
+tokenize_test_() ->
     [
      ?_assertEqual([], stream:to_list(tokenize(""))),
      ?_assertEqual(["0"], stream:to_list(tokenize("0"))),
      ?_assertEqual(["01"], stream:to_list(tokenize("01"))),
      ?_assertEqual(["0", "1"], stream:to_list(tokenize("0 1"))),
-     ?_assertEqual(["01", "23"], stream:to_list(tokenize("01 23")))
+     ?_assertEqual(["01", "23"], stream:to_list(tokenize("01 23"))),
+     ?_assertEqual(["(", "foo", ")"], stream:to_list(tokenize("(foo)")))
     ].
 
-read_token_test_() ->
+evaluate_test_() ->
     [
-     ?_assertEqual(0, read_token("0")),
-     ?_assertEqual(999, read_token("999")),
-     ?_assertEqual(-1, read_token("-1")),
-     ?_assertEqual(1, read_token("+1")),
-     ?_assertEqual(999, read_token("+999"))
+     ?_assertEqual(0, evaluate_token("0")),
+     ?_assertEqual(999, evaluate_token("999")),
+     ?_assertEqual(-1, evaluate_token("-1")),
+     ?_assertEqual(1, evaluate_token("+1")),
+     ?_assertEqual(999, evaluate_token("+999")),
+     ?_assertEqual('(', evaluate_token("(")),
+     ?_assertEqual(')', evaluate_token(")")),
+     ?_assertEqual(foo, evaluate_token("foo"))
     ].
