c6a23e51d4a8e15d310045426125103f56251025
Peter Winton <pwinton@indigobio.com>
2014-07-06 20:35:58 -0400

    bs_env:set

--- src/bs_env.erl
+++ src/bs_env.erl
@@ -1,29 +1,49 @@
 -module(bs_env).
 -include_lib("eunit/include/eunit.hrl").
--export([empty/0, lookup/2, extend/3]).
+-export([empty/0, lookup/2, extend/3, set/3]).
 
-empty() ->
-    {bs_env, [], [], nil}.
+make(Vars, Vals, Parent) ->
+    {bs_env, Vars, box:make(Vals), Parent}.
+
+empty() -> make([], [], nil).
 
 lookup(Var, nil) ->
-    error({not_defined, Var});
+    error({undefined_identifier, Var});
 
-lookup(Var, {bs_env, Vars, Vals, Parent}) ->
-    case lookup_val(Var, Vars, Vals) of
+lookup(Var, {bs_env, Vars, ValsBox, Parent}) ->
+    Vals = box:get(ValsBox),
+    case lookup_var(Var, Vars, Vals) of
         'BS_ENV_NO_VAL' ->
             lookup(Var, Parent);
         Val -> Val
     end.
 
-lookup_val(_, [], []) -> 'BS_ENV_NO_VAL';
-lookup_val(Var, [Var|_], [Val|_]) -> Val;
-lookup_val(Var, [_|Vars], [_|Vals]) -> lookup_val(Var, Vars, Vals).
+lookup_var(_, [], []) -> 'BS_ENV_NO_VAL';
+lookup_var(Var, [Var|_], [Val|_]) -> Val;
+lookup_var(Var, [_|Vars], [_|Vals]) -> lookup_var(Var, Vars, Vals).
 
-extend(Env, Vars, Vals) ->
-    {bs_env, Vars, Vals, Env}.
+extend(Env, Vars, Vals) -> make(Vars, Vals, Env).
+
+set(nil, Var, _) -> error({undefined_identifier, Var});
+
+set({bs_env, Vars, ValsBox, Parent}, Var, NewVal) ->
+    Vals = box:get(ValsBox),
+    case set_var(Vars, Vals, Var, NewVal) of
+        not_found -> set(Parent, Var, NewVal);
+        NewVals -> box:set(ValsBox, NewVals),
+                   ok
+    end.
+
+set_var([], [], _, _) -> not_found;
+set_var([Var|_], [_|RestVals], Var, NewVal) -> [NewVal|RestVals];
+set_var([_|Vars], [OldVal|Vals], Var, NewVal) ->
+    case set_var(Vars, Vals, Var, NewVal) of
+        not_found -> not_found;
+        Result -> [OldVal|Result]
+    end.
 
 lookup_empty_test() ->
-    ?assertError({not_defined, x}, lookup(x, empty())).
+    ?assertError({undefined_identifier, x}, lookup(x, empty())).
 
 lookup_test_() ->
     Env1 = extend(empty(), [a, b], [1, 2]),
@@ -48,4 +68,21 @@ shadow_test_() ->
      ?_assertEqual(4, lookup(c, Env2))
     ].
 
-     
+set_undefined_test() ->
+    ?assertError({undefined_identifier, x}, set(empty(), x, 1)).
+
+set_test_() ->
+    Env1 = extend(empty(), [a, b], [1, 2]),
+    Env2 = extend(Env1, [b, c], [3, 4]),
+    set(Env2, a, 11),
+    set(Env2, b, 33),
+    set(Env2, c, 44),
+    [
+     ?_assertEqual(11, lookup(a, Env2)),
+     ?_assertEqual(33, lookup(b, Env2)),
+     ?_assertEqual(44, lookup(c, Env2)),
+
+     ?_assertEqual(11, lookup(a, Env1)),
+     ?_assertEqual(2, lookup(b, Env1))
+    ].
+    
