1e1abaa3d97cf62a93f75e51ec96f15dc8468b0b
Peter Winton <pwinton@indigobio.com>
2014-08-13 21:10:21 -0400

    'fact'

--- src/bs_integration.erl
+++ src/bs_integration.erl
@@ -7,10 +7,7 @@ apply_lambda_test() ->
     ?assertEqual(2, Evaluated).
 
 set_top_level_test() ->
-    Env = bs_env:empty(),
-    Exprs = bs_read:read_all("(set! x 5)   x"),
-    Result = lists:foldl(fun(Expr, _) -> bs_compile:eval(Expr, Env) end, nil, Exprs),
-    ?assertEqual(5, Result).
+    ?assertEqual(5, eval("(set! x 5)   x")).
 
 primop_test() ->
     Expr = bs_read:read1("(+ (- 7 4) 10)"),
@@ -22,3 +19,11 @@ if_test() ->
     Expr = bs_read:read1("(if (quote a) 1 2)"),
     ?assertEqual(1, bs_compile:eval(Expr)).
 
+eval(S) ->
+    Env = bs_scheme:env(),
+    Exprs = bs_read:read_all(S),
+    Result = lists:foldl(fun(Expr, _) -> bs_compile:eval(Expr, Env) end, nil, Exprs),
+    Result.
+
+fact_test() ->
+    ?assertEqual(120, eval("(set! fact (lambda (x) (if (= x 1) 1 (* x (fact (- x 1))))))   (fact 5)")).
--- src/bs_scheme.erl
+++ src/bs_scheme.erl
@@ -1,9 +1,19 @@
 -module(bs_scheme).
 -export([env/0]).
+-include("bs_const.hrl").
+
+bool(true) -> ?TRUE;
+bool(false) -> ?FALSE.
+
+compose(F, G) ->
+    fun(X) -> F(G(X)) end.
+            
 
 env() ->
     E = bs_env:empty(),
     bs_env:set(E, '+', fun erlang:'+'/2),
     bs_env:set(E, '-', fun erlang:'-'/2),
+    bs_env:set(E, '*', fun erlang:'*'/2),
+    bs_env:set(E, '=', fun(A, B) -> bool(A == B) end),
     E.
     
